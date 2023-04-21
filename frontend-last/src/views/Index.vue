f13d00b3ab629c4466536b0a481c61ba24276ec4
Innocence <99720756+BUCTLjm@users.noreply.github.com>
2023-04-22 00:26:07 +0800

    last last commit
    real frontend code is here
    2023/4/22

---
+++ frontend-last/src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>欢迎{{$route.query.username}}来到首页</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name:'index'
+}
+</script>
+<style scoped>
+
+</style>
