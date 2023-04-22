07eba6eea3274ebb409ef66f5c8078ac842194b3
zpf <1600493127@qq.com>
2023-04-22 17:32:10 +0800

    delete useless files

--- front-end final/src/api/email.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function sendVerifyCode(email) {
-    return request({
-        url: '/email/verifyCode',
-        method: 'GET',
-        params: {email}
-    })
-}
